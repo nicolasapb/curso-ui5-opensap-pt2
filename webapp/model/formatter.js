5855eaad903b57663ba88885b802c26205a2f0fc
Barbosa <nicolas.a.barbosa@accenture.com>
2018-09-13 14:38:31 -0300

    ui5 4.3

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -29,7 +29,33 @@ sap.ui.define([
 			formatMapUrl: function(sStreet, sZIP, sCity, sCountry) {
 				return "https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=640x640&markers=" 
 					+ jQuery.sap.encodeURL(sStreet + ", " + sZIP + "" + sCity + ", " + sCountry); 
-			}
+			},
+
+			/**
+			 * @public
+			 * Determines a delivery method based on the weight of a product
+			 * @param {string} sMeasure the measure of the weight to be formatted
+			 * @param {integer} iWeight the weight to be formatted
+			 * @returns {string} sValue the delivery method
+			 */
+			delivery: function (sMeasure, iWeight) {
+				var oResourceBundle = this.getModel("i18n").getResourceBundle(),
+					sResult = "";
+
+				if (sMeasure === "G") {
+					iWeight = iWeight / 1000;
+				}
+				if (iWeight < 0.5) {
+					sResult = oResourceBundle.getText("formatterMailDelivery");
+				} else if (iWeight < 5) {
+					sResult = oResourceBundle.getText("formatterParcelDelivery");
+				} else {
+					sResult = oResourceBundle.getText("formatterCarrierDelivery");
+				}
+
+				return sResult;
+			},
+
 
 
 		};
